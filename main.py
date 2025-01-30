3183963c3ea7a3fff208d2689108b6a9f53d2dcb
Bakhodir03 <ikromovbahodir94@gmail.com>
2025-01-30 15:22:03 +0500

    birinchi yuklash

---
+++ main.py
@@ -0,0 +1,57 @@
+# #1-misol
+# a = float(input("a sonni kiritin:a="))
+# b = float(input("b sonni kiriting:b="))
+#
+# while a>=b:
+#     a-=b
+# print(a)
+#2-misol
+# a = float(input("a sonni kiritin:a="))
+# b = float(input("b sonni kiriting:b="))
+# soni=0
+# while a>b:
+#     a-=b
+#     soni+=1
+# print(soni)
+#3-misol
+# N = float(input("N sonni kiritin:a="))
+# K = float(input("K sonni kiriting:b="))
+#
+# while N>=K:
+#     N-=K
+# print(N)
+#4-misol
+# n = float(input("N sonni kiritin:a="))
+# while n>0:
+#     n-=3
+# if n==0:
+#     print( "3 ning darajasi")
+#
+# else:
+#      print("emas")
+#5-misol
+# n = int(input("N sonini kiriting: "))
+#
+# m = 1
+# daraja = 0
+#
+# while m < n:
+#     m *= 2
+#     daraja += 1
+#
+# if m == n:
+#     print(daraja)
+# else:
+#     print("Bu son 2 ning darajasi emas.")
+
+#6-misol
+# n = int(input("N sonini kiriting: "))
+# naija=1
+# while n>0:
+#     naija*=n
+#     n-=2
+# print(naija)
+#7-misol
+n = int(input("N sonini kiriting: "))
+while n>0:
+    
